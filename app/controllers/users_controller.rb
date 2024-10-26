da6a6df58f8550d9fb6a9204367eea7fa1af20c2
Tclewis13 <the3rdi@gmail.com>
2024-10-26 00:17:11 -0400

    project done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(allowed_user_params)
     if @user.save
       redirect_to new_user_path
     else
@@ -13,9 +13,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(allowed_user_params)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
-  def allowed_post_params
-    params.require(:post).permit(:username, :password, :email)
+  def allowed_user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :password, presence: true
+  validates :username, presence: true, length: { minimum: 2 }
   validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
 end
